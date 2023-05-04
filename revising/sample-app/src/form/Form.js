0ae4009e6ccefdedb6d4d98742e9f301484d6e5c
jeeves1618 <jeeves1618@gmail.com>
2023-05-04 22:47:11 +0530

    Dynamic Styling

--- revising/sample-app/src/form/Form.js
+++ revising/sample-app/src/form/Form.js
@@ -3,13 +3,18 @@ import "./Form.css";
 
 function Form(props) {
   const [enteredValue, setEnteredValue] = useState("");
-
+  const [isValidTitle, setIsValidTitle] = useState("true");
   function newTitleHandler(event) {
+    setIsValidTitle(true);
     setEnteredValue(event.target.value);
   }
 
   function addToCatalogueHandler(event) {
     event.preventDefault(); //This is preventing the reload of the page. It retains the below console log.
+    if (enteredValue.trim().length === 0) {
+      setIsValidTitle(false);
+      return;
+    }
     props.onAddBook(enteredValue);
     setEnteredValue("");
   }
@@ -17,15 +22,22 @@ function Form(props) {
   return (
     <form onSubmit={addToCatalogueHandler}>
       <div className="formSection">
-        <h2>Please enter the title of the book to be added to catalogue</h2>
+        <h2 style={{ color: !isValidTitle ? "red" : "black" }}>
+          Please enter the title of the book to be added to catalogue
+        </h2>
         <input
           type="text"
           size="80"
           value={enteredValue}
           onChange={newTitleHandler}
+          style={{
+            backgroundColor: !isValidTitle ? "#E7BFBF" : "white",
+            borderColor: !isValidTitle ? "red" : "black",
+          }}
         ></input>
-        <button type="submit">Add to catalogue</button>
-
+        <div className={`form-control ${!isValidTitle ? "invalid" : ""}`}>
+          <button type="submit">Add to catalogue</button>
+        </div>
         <p></p>
         <hr></hr>
       </div>
